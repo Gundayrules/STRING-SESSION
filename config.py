95388d9b11ae614d687b91a4b6847787a38148be
Atul tiwari <162774395+Gundayrules@users.noreply.github.com>
2024-05-29 11:05:15 +0530

    Update config.py
    Ladli

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 #OWNER_ID = int(getenv("6752444356"))
-OWNER_ID = int(getenv("OWNER_ID", "6752444356"))
+OWNER_ID = int(getenv("OWNER_ID", "6708805293"))
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 MUST_JOIN = getenv("MUST_JOIN", None)
